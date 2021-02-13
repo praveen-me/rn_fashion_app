a5dcc704f933cf32cda240a53e4b7c42c82db363
praveen-me <praveenkrsaini0@gmail.com>
2021-02-13 14:42:37 +0530

    Added icon and splash screen for android

--- App.tsx
+++ App.tsx
@@ -5,8 +5,13 @@ import {ThemeProvider} from '@shopify/restyle';
 import theme from './src/contants/theme';
 import RootNavigator from './src/lib/navigation/rootNavigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
--- android/app/src/main/java/com/rn_fashion_app/MainActivity.java
+++ android/app/src/main/java/com/rn_fashion_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rn_fashion_app;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "rn_fashion_app";
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 }

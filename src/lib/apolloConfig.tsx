8cfc8dacfd3ecd5e8919bde13294aa689aad0b1d
praveen-me <praveenkrsaini0@gmail.com>
2021-11-11 23:57:02 +0530

    Remove unncessary logs

--- src/lib/apolloConfig.tsx
+++ src/lib/apolloConfig.tsx
@@ -1,8 +1,6 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import Config from 'react-native-config';
 
-console.log('uri', Config.GRAPHQL_URI);
-
 const config = new ApolloClient({
   uri: Config.GRAPHQL_URI as string,
   cache: new InMemoryCache(),
